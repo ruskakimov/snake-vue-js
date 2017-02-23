086966d06170c5d3d5a341c4b4f93eba125b9e3d
Rustem Kakimov <ruskakimov@gmail.com>
2017-02-23 22:50:31 +0800

    Add blink and spacekey visual components

---
+++ src/components/Blink.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="blink">
+    <span>press</span>
+    <slot name="key">
+      (key visual here)
+    </slot>
+    <span>to {{ actionName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blink',
+  props: {
+    actionName: String
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .blink {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: blink 1s infinite;
+  }
+  @keyframes blink {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.5;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/components/SpaceKey.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="space">
+    <kbd>Space</kbd>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'space-key'
+}
+</script>
+
+<style lang="css" scoped>
+  .space {
+    margin: 0 0.5em;
+    position: relative;
+    font-size: 1em;
+    width: 5em;
+    height: 1.8em;
+    border-radius: 0.2em;
+    background: #eee;
+  }
+  .space kbd {
+    position: absolute;
+    left: 0.5em;
+    bottom: 0.4em;
+  }
+</style>
