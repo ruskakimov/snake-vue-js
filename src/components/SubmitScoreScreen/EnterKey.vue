470a3481af248b07a8846d1d36e3dc43387029e5
Rustem Kakimov <ruskakimov@gmail.com>
2017-02-24 20:08:20 +0800

    Style enter key visual component

--- src/components/SubmitScoreScreen/EnterKey.vue
+++ src/components/SubmitScoreScreen/EnterKey.vue
@@ -11,18 +11,32 @@ export default {
 </script>
 
 <style lang="css" scoped>
+  .enter,
+  .enter::after {
+    border-radius: 0.2em;
+    background: #eee;
+  }
   .enter {
     position: relative;
     font-size: 1em;
-    width: 5em;
+    width: 4em;
     height: 1.8em;
-    border-radius: 0.2em;
-    background: #eee;
+  }
+  .enter::after {
+    content: '';
+    display: block;
+    height: 3.6em;
+    width: 2.5em;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    z-index: 4;
   }
   .enter kbd {
     color: #333;
     position: absolute;
     left: 0.5em;
     bottom: 0.4em;
+    z-index: 5;
   }
 </style>
