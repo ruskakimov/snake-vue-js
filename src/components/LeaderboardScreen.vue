1b8be2cccbbad0b9bf8b9e730196375c28b888b8
Rustem Kakimov <ruskakimov@gmail.com>
2017-02-24 16:36:57 +0800

    Add leaderboard screen component

---
+++ src/components/LeaderboardScreen.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="screen">
+    <leaderboard></leaderboard>
+    <blink actionName="restart">
+      <space-key slot="key"></space-key>
+    </blink>
+  </div>
+</template>
+
+<script>
+import { scoreboard } from './../db'
+import Leaderboard from './Leaderboard'
+import Blink from './Blink'
+import SpaceKey from './SpaceKey'
+
+export default {
+  name: 'leaderboard-screen',
+  components: {
+    Leaderboard, Blink, SpaceKey
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .screen {
+    width: 100%;
+    height: 100%;
+  }
+</style>
